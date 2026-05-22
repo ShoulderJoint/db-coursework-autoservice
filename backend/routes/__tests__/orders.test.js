3aa38c93958fdf7656b39f692b1c673ea9b42d7a
ShoulderJoint <misha.kondratev.2005@mail.ru>
2026-05-22 18:14:45 +0400

    added more unit tests to order.test.js

--- backend/routes/__tests__/orders.test.js
+++ backend/routes/__tests__/orders.test.js
@@ -1,11 +1,19 @@
 const request = require('supertest');
 const app = require('../../app');
+const { result } = require('../../db');
 
 test('Проверка метода get /orders по получению всех Заказ-нарядов', async () => {
     await request(app)
     .get('/orders')
     .expect('Content-Type', /json/)
     .expect(200);
+});
+
+test('Проверка метода get /orders:id по получению несуществующего ЗН', async () => {
+    const notExistedOrderId=1233;
+    await request(app)
+    .get(`/orders/${notExistedOrderId}`)
+    .expect(500);
 });
 
 test('Проверка метода post /orders по добавлению Заказ-наряда', async () => {
@@ -31,4 +39,17 @@ test('Проверка метода put /orders:id/status по обновлен�
         .send(updatedData)
         .expect('Content-Type', /json/)
     expect(res.statusCode).toBe(200);
+});
+
+test('Проверка метода post /orders:id/parts по добавлению комплектующих в Заказ-наряд', async () => {
+    const workOrderId=9;
+    addedData={
+        customName: "Мотороное Масло CompanyName", 
+        isClientProvided: true
+    };
+    const res=await request(app)
+        .post(`/orders/${workOrderId}/parts`)
+        .send(addedData)
+        .expect('Content-Type', /json/)
+    expect(res.statusCode).toBe(201);
 });
