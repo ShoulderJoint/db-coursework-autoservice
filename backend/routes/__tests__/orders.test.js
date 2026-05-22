185e0fdc7e8382a64827ac5d1a399e5356e209c2
ShoulderJoint <misha.kondratev.2005@mail.ru>
2026-05-22 19:01:11 +0400

    refactored unit-tests for orders

--- backend/routes/__tests__/orders.test.js
+++ backend/routes/__tests__/orders.test.js
@@ -2,24 +2,26 @@ const request = require('supertest');
 const app = require('../../app');
 const { result } = require('../../db');
 
-test('Проверка метода get /orders по получению всех Заказ-нарядов', async () => {
-    await request(app)
-    .get('/orders')
-    .expect('Content-Type', /json/)
-    .expect(200);
+describe('Get orders', () => {
+    it('return 200 and json', async () => {
+        await request(app)
+            .get('/orders')
+            .expect('Content-Type', /json/)
+            .expect(200);
+    });
+
+    it('return 500', async () => {
+        const notExistedOrderId = 1233;
+        await request(app)
+            .get(`/orders/${notExistedOrderId}`)
+            .expect(500);
+    });
 });
 
-test('Проверка метода get /orders:id по получению несуществующего ЗН', async () => {
-    const notExistedOrderId=1233;
-    await request(app)
-    .get(`/orders/${notExistedOrderId}`)
-    .expect(500);
-});
-
-test('Проверка метода post /orders по добавлению Заказ-наряда', async () => {
+test('Post order', async () => {
     newOrder = {
-        applicationId: 2, 
-        staffId: 5,   
+        applicationId: 2,
+        staffId: 5,
         services: 5
     };
     const res = await request(app)
@@ -29,9 +31,9 @@ test('Проверка метода post /orders по добавлению За�
     expect(res.statusCode).toBe(201);
 });
 
-test('Проверка метода put /orders:id/status по обновлению статуса Заказ-наряда', async () => {
-    const updatedOrderId=9;
-    const updatedData={
+test('Put status in order', async () => {
+    const updatedOrderId = 9;
+    const updatedData = {
         statusId: 2
     };
     const res = await request(app)
@@ -41,13 +43,13 @@ test('Проверка метода put /orders:id/status по обновлен�
     expect(res.statusCode).toBe(200);
 });
 
-test('Проверка метода post /orders:id/parts по добавлению комплектующих в Заказ-наряд', async () => {
-    const workOrderId=9;
-    addedData={
-        customName: "Мотороное Масло CompanyName", 
+test('Post parts in order', async () => {
+    const workOrderId = 9;
+    addedData = {
+        customName: "Моторное Масло CompanyName",
         isClientProvided: true
     };
-    const res=await request(app)
+    const res = await request(app)
         .post(`/orders/${workOrderId}/parts`)
         .send(addedData)
         .expect('Content-Type', /json/)
