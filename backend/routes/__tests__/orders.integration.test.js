439b52fb59b6d2b6b5a23f555090eedd6c2a6f15
ShoulderJoint <misha.kondratev.2005@mail.ru>
2026-05-29 22:32:38 +0400

    final, but not a last

--- backend/routes/__tests__/orders.integration.test.js
+++ backend/routes/__tests__/orders.integration.test.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 
 const generateTestToken = (userId, role) => {
 
-    const secret = process.env.JWT_SECRET || '5c8140793205e9c264d7bdf2a7bf62dbabfb67e584872f9d66686bcef4a402ee92b30f0361f70cd3d2a52699545259d5f383ea421e2f7b7079a13f09eacab1a2';
+    const secret = process.env.JWT_SECRET;
 
     return jwt.sign({ id: userId, role: role }, secret, { expiresIn: '1h' });
 };
